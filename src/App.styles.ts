62ea08eb779a2573617affaddbaff837ae22780b
Yuji Sato <yujisatojr@gmail.com>
2021-06-20 21:06:42 -1000

    Use function component for question card and add more styling to App

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -2,3 +2,19 @@ import styled, { createGlobalStyle } from 'styled-components';
 import BGImage from './images/nattu-adnan-unsplash.jpg';
 
 export const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+  body {
+    background-image: url(${BGImage});
+    background-size: cover;
+    margin: 0;
+    padding: 0 20px;
+    display: flex;
+    justify-content: center;
+  }
+  * {
+    font-family: 'Catamaran', sans-serif;
+    box-sizing: border-box;
+  }
+`;
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -10,3 +10,34 @@ type Props = {
   questionNr: number;
   totalQuestions: number;
 };
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <Wrapper>
+    <p className='number'>
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <div>
+      {answers.map((answer) => (
+        <ButtonWrapper
+          key={answer}
+          correct={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer}
+        >
+          <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </ButtonWrapper>
+      ))}
+    </div>
+  </Wrapper>
+);
+
+export default QuestionCard;
