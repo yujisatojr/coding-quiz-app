f2f59254f5a4b8d434d0c7c683113b619d0033a7
Yuji Sato <yujisatojr@gmail.com>
2021-06-20 20:44:00 -1000

    Add checkAnswer method

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,25 @@ const App: React.FC = () => {
     setNumber(0);
     setLoading(false);
   };
+
+  const checkAnswer = (e: any) => {
+    if (!gameOver) {
+      // User's answer
+      const answer = e.currentTarget.value;
+      // Check answer against correct answer
+      const correct = questions[number].correct_answer === answer;
+      // Add score if answer is correct
+      if (correct) setScore((prev) => prev + 1);
+      // Save the answer in the array for user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answerObject]);
+    }
+  };
 };
 
 export default App;
