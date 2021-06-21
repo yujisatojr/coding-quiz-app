b08d53ce590260fe9315b8842af10a7c49a1e512
Yuji Sato <yujisatojr@gmail.com>
2021-06-20 20:57:01 -1000

    Add next question method and return frontend code

--- src/App.tsx
+++ src/App.tsx
@@ -36,13 +36,9 @@ const App: React.FC = () => {
 
   const checkAnswer = (e: any) => {
     if (!gameOver) {
-      // User's answer
       const answer = e.currentTarget.value;
-      // Check answer against correct answer
       const correct = questions[number].correct_answer === answer;
-      // Add score if answer is correct
       if (correct) setScore((prev) => prev + 1);
-      // Save the answer in the array for user answers
       const answerObject = {
         question: questions[number].question,
         answer,
@@ -52,6 +48,47 @@ const App: React.FC = () => {
       setUserAnswers((prev) => [...prev, answerObject]);
     }
   };
+
+  const nextQuestion = () => {
+    const nextQ = number + 1;
+
+    if (nextQ === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQ);
+    }
+  };
+
+  return (
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <h1>REACT QUIZ</h1>
+        {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+          <button className='start' onClick={startTrivia}>
+            Start
+          </button>
+        ) : null}
+        {!gameOver ? <p className='score'>Score: {score}</p> : null}
+        {loading ? <p>Loading Questions...</p> : null}
+        {!loading && !gameOver && (
+          <QuestionCard
+            questionNr={number + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+          />
+        )}
+        {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+          <button className='next' onClick={nextQuestion}>
+            Next Question
+          </button>
+        ) : null}
+      </Wrapper>
+    </>
+  );
 };
 
 export default App;
