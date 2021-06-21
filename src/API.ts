1d64253a5aa9b6672eef3f78c40de0f036c4e78b
Yuji Sato <yujisatojr@gmail.com>
2021-06-20 20:10:35 -1000

    Retrieve quiz questions from API

--- src/API.ts
+++ src/API.ts
@@ -16,3 +16,12 @@ export enum Difficulty {
 }
 
 export type QuestionsState = Question & { answers: string[] };
+
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty): Promise<QuestionsState[]> => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
+  }))
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { fetchQuizQuestions } from './API';
+import QuestionCard from './components/QuestionCard';
+import { QuestionsState, Difficulty } from './API';
 
 function App() {
   return <div className='App'>Quiz</div>
