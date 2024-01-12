3f0b3995531570e6dbe402dacccd87ccbbf9d581
Yuji Sato <yujisatojr@gmail.com>
2024-01-11 23:30:30 -0600

    Change app name

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ const App: React.FC = () => {
     <>
       <GlobalStyle />
       <Wrapper>
-        <h1>Programming Quiz</h1>
+        <h1>Coding Quiz</h1>
         {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
           <button className='start' onClick={startTrivia}>
             Start
